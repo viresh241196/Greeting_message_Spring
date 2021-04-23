a9138a479f324f1b597fe35b4779050db2f615f0
viresh241196 <vireshrawool007@gmail.com>
2021-04-23 23:36:50 +0530

    added method for greeting and added a class

---
+++ main/java/com/example/demo/controller/Greeting.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+public class Greeting {
+	public String message;
+	public long id;
+
+	public Greeting(long id,String message) {
+		this.id= id;
+		this.message= message;
+	}
+
+}
